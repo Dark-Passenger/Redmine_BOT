1bb4f5c1fab598dfd7452cc885872d95176ae245
Dhruv Paranjape <lord.dhruv@gmail.com>
2016-09-26 12:48:05 +0530

    Update to add late entry support and other bug fixes
    Add support for late entry.
    Add support to skip weekends(here assumed to be saturday or sunday)

--- redminebot.py
+++ redminebot.py
@@ -1,13 +1,23 @@
 from sqlite3 import connect
 from redmine import Redmine
 from datetime import date, timedelta
+from calendar import day_name
 from random import choice
 from os import remove
 from sys import argv
 
-if len(sys.argv) < 2:
-    print("usage : ",argv[0]," < issue / time >")
+if len(argv) > 1:
+    run = argv[1]
+else:
+    print("Oh no you didnt tell me what to do ...")
+    print("usage : ",argv[0]," <issue/time>")
     exit()
+
+#print("Login : ")
+#username = input("Enter your redmine username : ")
+username = 'dhruv.paranjape'                #hardcode it here but when user_id and user_key has more entries make this input based selection.
+user = username.split('.')[0].title()
+print("Hello ",user)
 
 conn = connect('redmine.db')
 
@@ -17,32 +27,47 @@ conn.execute('''CREATE TABLE IF NOT EXISTS ISSUES
        SUBJECT       TEXT       NOT NULL,
        HOURS         INT        NOT NULL,
        ACTIVITY      TEXT       NOT NULL,
-       TEAM          TEXT       NOT NULL
+       TEAM          TEXT       NOT NULL,
        END_DATE      TEXT       NOT NULL);''')
 
-user_id = 
-user_key = 
-projects = 
-task_id = 
-status_id = 
-activity_id = 
+user_id = {'dhruv.paranjape' : 598}
+user_key = {'dhruv.paranjape' :'4ff211d2eb10cda87af5001049c6115112363562'}
+projects = {'Development' : 431, 'UTM' : 112, '1.8.0' : 306, '2.0.0' : 308}
+task_id = {'QA-Task': 10,'Dev-Task' : 9}
+status_id = {'In Progress' : 2, 'Completed' : 13, 'Closed' : 5}
+activity_id = {"Design" : 8, "Documentation" : 26, "Coding" : 9, "Meeting" : 16, "KT" : 17, "Code Review" : 18, "Test Design" : 27, "Test Execution" : 28, "Test Review" : 29, "Analysis" : 43, "Test Plan creation" : 133}
 
-print("Hello User")
-run = argv[1].lower()
-
-red = Redmine('<redmine_url>', key=user_key['<user>'])
+red = Redmine('http://projects.qhtpl.com/redmine', key=user_key[username])
 
 cursor = conn.cursor()
 
-today = date.today()
+def IssueCreator(late_counter = 0):
 
+    today = date.today() - timedelta(days=late_counter)
 
-if run == 'issue':
     print("Today is ",today)
+
+    if day_name[today.weekday()].lower() == "saturday" or day_name[today.weekday()].lower() == "sunday":
+        print(today, " is a weekend skipping")
+        return 1
+
     subject = input("Subject is : ")
     #Dumb-Dumb starts here
-    # keywords detection and setting parameters based on that.
-    # scrubbed due to possible Insider info
+    if subject.lower().find('automation') != -1 or subject.lower().find('creat') != -1 or subject.lower().find('cod') != -1:
+        task = 'QA-Task'
+        Team = 'QA'
+        Activity = "Coding"
+        project = '2.0.0'
+    elif subject.lower().find('test') != -1 or subject.lower().find('verif') != -1 or subject.lower().find('bug') != -1:
+        task = 'QA-Task'
+        Team = 'QA'
+        Activity = "Test Execution"
+        project = '1.8.0'
+    else:
+        task = 'QA-Task'
+        Team = 'QA'
+        Activity = "Test Execution"
+        project = '1.8.0'
 
     duration = int(input("duration : <In days>"))
     future = today + timedelta(days=duration)
@@ -50,7 +75,7 @@ if run == 'issue':
 
     print("Creating issue  for : ",today)
 
-    issue = red.issue.create(project_id=projects[project], subject=subject, tracker_id=task_id[task], assigned_to_id=user_id['<user>'], start_date=today, due_date=future, estimated_hours=time, done_ratio=0)
+    issue = red.issue.create(project_id=projects[project], subject=subject, tracker_id=task_id[task], assigned_to_id=user_id[username], start_date=today, due_date=future, estimated_hours=time, done_ratio=0)
     
     print("issue with id : ",issue.id," created")
     print("Saving issue ...")
@@ -58,9 +83,12 @@ if run == 'issue':
     cursor.execute("""INSERT INTO ISSUES (ISSUE_ID, SUBJECT, HOURS ,ACTIVITY ,TEAM, END_DATE) VALUES (?,?,?,?,?,?)""", (issue.id, subject, round(time), Activity, Team, future))
     conn.commit()
     print("Issue saved")
-    conn.close()
-    print("All done")
-    print("Bye !")
+    if run != "late":
+        conn.close()
+
+if run == 'issue':
+
+    IssueCreator()
 
 elif run == 'time' :
 
@@ -72,7 +100,7 @@ elif run == 'time' :
         duration = wordlist[3]
         activity = wordlist[4]
         team     = wordlist[5]
-        end_date = wordlist[6]
+        end_time = wordlist[6]
 
         print("Processing "+wordlist[2]+" Issue now")
 
@@ -84,13 +112,13 @@ elif run == 'time' :
         else:
             print("Issue failed to update")
         
-        coin_toss = choice([-1, 0, 1])
+        coin_toss = choice([0, 1, 2])
 
         time_spent = duration + coin_toss
         
         print("Entering time now ...")
         
-        issue = red.time_entry.create(issue_id=issue_id, spent_on=end_date, hours=time_spent, activity_id=activity_id[activity], custom_fields=[{'id': 64, 'value': team}])
+        issue = red.time_entry.create(issue_id=issue_id, spent_on=end_time, hours=time_spent, activity_id=activity_id[activity], custom_fields=[{'id': 64, 'value': team}])
         
         print("time entry for issue id : ",issue_id," added")
 
@@ -106,9 +134,17 @@ elif run == 'time' :
     
     remove('redmine.db')
     print(" Database Removed")
-    print("All done")
-    print("Bye !")
 
+elif run == "late":
+    print("you are using a secret feature ! shhhh")
+    late_counter = int(input("how late are you ?"))
+    
+    while late_counter > -1:
+        IssueCreator(late_counter)
+        late_counter -= 1
 else :
     print("You chose poorly #ThePrincessBride")
     print("Shutting down now ...")
+
+print("All done")
+print("Bye !")
